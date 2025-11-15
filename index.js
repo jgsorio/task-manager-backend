c49f63724d80b5d1c50c9c0a085320801d066084
José Guilherme <joseguilhermesorio@gmail.com>
2025-11-15 15:57:43 -0300

    feat: add cors and add port to environment

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv/config');
 const express = require('express');
+const cors = require('cors');
 const connectToDatabase = require('./src/database/mongoose.database'); 
 const taskRoutes = require('./src/routes/task.route');
 
 const app = express();
 connectToDatabase();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
-app.listen(8000, () => console.info('listening on port 8000'));
+app.listen(process.env.PORT, () => console.info(`listening on port ${process.env.PORT}`));
